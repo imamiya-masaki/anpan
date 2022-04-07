b1429fb2f3b0d792658197aac8abec85c418ff47
imamiya-masaki <imamiyagit@gmail.com>
2022-04-08 01:30:31 +0900

    lighthousec

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,8 @@
 module.exports = {
     ci: {
         "collect": {
-            "numberOfRuns": 1,
-            "startServerCommand": "npm run start",
+            numberOfRuns: 1, // Lighthouse の試行回数は1回
+            startServerCommand: 'npm run start', // プロダクションモードでローカルサーバーを起動する
             "url": [
               "http://localhost:3000/",
             ]
