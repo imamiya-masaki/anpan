cf25c2de5b4e45c429e4c6f2a1f4ca1cef41c565
imamiya-masaki <imamiyagit@gmail.com>
2022-04-08 01:11:25 +0900

    test

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,12 @@
 module.exports = {
     ci: {
+        "collect": {
+            "numberOfRuns": 1,
+            "startServerCommand": "npm run start",
+            "url": [
+              "http://localhost:3000/",
+            ]
+          },
       upload: {
         target: 'temporary-public-storage',
       },
